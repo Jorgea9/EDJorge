098efb8464b95cba7d6a625cd4a80d07a6929f4d
jorge <jorgea9@yahoo.es>
2021-02-07 14:13:13 +0100

    Actividad 1 Videojuego

---
+++ OneDrive/Desktop/DAM/ENTORNOS DE DESARROLLO/Actividad1/Videojuego.java
@@ -0,0 +1,28 @@
+package entidad;
+
+public class Videojuego {
+	public String nombre;
+	public String compañia;
+	public int id;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCompañia() {
+		return compañia;
+	}
+	public void setCompañia(String compañia) {
+		this.compañia = compañia;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
