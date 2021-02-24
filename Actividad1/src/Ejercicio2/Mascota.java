3a132cdd4d1f57e03dd01c346417576013cca53a
Peter <peter29041@gmail.com>
2021-02-24 18:19:49 +0100

    tercer commit

--- Actividad1/src/Ejercicio2/Mascota.java
+++ Actividad1/src/Ejercicio2/Mascota.java
@@ -5,6 +5,24 @@ public class Mascota {
 	private String nombre;
 	private String color;
 	private int id;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 	
 }
