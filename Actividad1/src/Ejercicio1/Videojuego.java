dfd2ad14cc424fb7b1e3672e1321744a45681f09
Peter <peter29041@gmail.com>
2021-02-24 18:13:24 +0100

    Primer commit

---
+++ Actividad1/src/Ejercicio1/Videojuego.java
@@ -0,0 +1,28 @@
+package Ejercicio1;
+
+public class Videojuego {
+	private int id;
+	private String nombre;
+	private String compañia;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCompañia() {
+		return compañia;
+	}
+	public void setCompañia(String compañia) {
+		this.compañia = compañia;
+	} 
+	
+	
+	
+}
