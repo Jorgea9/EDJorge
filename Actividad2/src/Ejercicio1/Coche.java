fcbb1f870dbe7cbbd7ec159e6fb9464e90e9f2cb
Peter <peter29041@gmail.com>
2021-02-27 14:54:19 +0100

    Segundo commit camaradas

--- Actividad2/src/Ejercicio1/Coche.java
+++ Actividad2/src/Ejercicio1/Coche.java
@@ -2,7 +2,50 @@ package Ejercicio1;
 
 public class Coche {
 
-	public static void main(String[] args) {
-		
+	private int id;
+	private String matricula;
+	private String modelo;
+	private String marca;
+	
+	public Coche(int id, String matricula, String modelo, String marca) {
+		super();
+		this.id = id;
+		this.matricula = matricula;
+		this.modelo = modelo;
+		this.marca = marca;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
 }
---
+++ Actividad2/src/Ejercicio1/Motor.java
@@ -0,0 +1,36 @@
+package Ejercicio1;
+
+public class Motor {
+
+	private int id;
+	private String tipoMotor;
+	private String potencia;
+	
+	
+	public Motor(int id, String tipoMotor, String potencia) {
+		super();
+		this.id = id;
+		this.tipoMotor = tipoMotor;
+		this.potencia = potencia;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTipoMotor() {
+		return tipoMotor;
+	}
+	public void setTipoMotor(String tipoMotor) {
+		this.tipoMotor = tipoMotor;
+	}
+	public String getPotencia() {
+		return potencia;
+	}
+	public void setPotencia(String potencia) {
+		this.potencia = potencia;
+	}
+	
+	
+}
