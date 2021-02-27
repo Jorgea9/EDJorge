ded85122ad2406fd0b9c760fff23cdc1da5847ea
Peter <peter29041@gmail.com>
2021-02-27 14:43:10 +0100

    PAPU TE QUIERO

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e7/b029c14bc476001b108ebd15a8ac4dba
@@ -0,0 +1,10 @@
+package Ejercicio2;
+
+public class Mascota {
+
+	private String nombre;
+	private String color;
+	private int id;
+	
+	
+}
---
+++ Actividad1/src/Ejercicio3/Coche.java
@@ -0,0 +1,9 @@
+package Ejercicio3;
+
+public class Coche {
+	
+	private int id ;
+	private String matricula;
+	private String modelo;
+	private String marca;
+}
